7a580dba87a9cec48b540704bbb24e47c12f2d8e
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-10-28 15:21:39 +0000

    feat: creada version preliminar de la app de turnos con la estructura del tutorial

--- web_app/lib/main.dart
+++ web_app/lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:math';
+
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:random_name_generator/random_name_generator.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,6 +18,7 @@ class MyApp extends StatelessWidget {
       create: (context) => MyAppState(),
       child: MaterialApp(
         title: 'Namer App',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(
@@ -26,7 +30,39 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Appointment {
+  Appointment();
+
+  var businessName = RandomNames(Zone.spain).name();
+  var serviceDescription = RandomNames(Zone.spain).surname();
+  DateTimeRange timeRange = DateTimeRange(
+    start: DateTime(
+      2024,
+      1,
+      30,
+      10,
+    ),
+    end: DateTime(
+      2024,
+      1,
+      30,
+      10,
+    ),
+  );
+  double servicePrice = Random().nextDouble() * 100;
+}
+
 class MyAppState extends ChangeNotifier {
+  var appointments = <Appointment>[];
+  bool isLoggedIn = false;
+
+  void bookAppointment(Appointment appointment) {
+    if (!appointments.contains(appointment)) {
+      appointments.add(appointment);
+    }
+    notifyListeners();
+  }
+
   var current = WordPair.random();
 
   void getNext() {
@@ -59,127 +95,147 @@ class _MyHomePageState extends State<MyHomePage> {
     Widget page;
     switch (selectedIndex) {
       case 0:
-        page = GeneratorPage();
+        page = MainPage();
       case 1:
-        page = FavoritesPage();
+        page = MyAppointmentsPage();
       default:
         throw UnimplementedError('no widget for $selectedIndex');
     }
 
     return LayoutBuilder(builder: (context, constraints) {
       return Scaffold(
-        body: Row(
-          children: [
-            SafeArea(
-              child: NavigationRail(
-                extended: constraints.maxWidth >= 600,
-                destinations: [
-                  NavigationRailDestination(
-                    icon: Icon(Icons.home),
-                    label: Text('Inicio'),
-                  ),
-                  NavigationRailDestination(
-                    icon: Icon(Icons.favorite),
-                    label: Text('Favoritos'),
-                  ),
-                ],
-                selectedIndex: selectedIndex,
-                onDestinationSelected: (value) {
-                  setState(() {
-                    selectedIndex = value;
-                  });
-                },
-              ),
-            ),
-            Expanded(
-              child: Container(
-                color: Theme.of(context).colorScheme.primaryContainer,
-                child: page,
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.onSecondary,
+          toolbarHeight: 80,
+          title: Row(
+            children: [
+              SizedBox(width: 25),
+              IconButton(
+                  tooltip: "Inicio",
+                  onPressed: () {
+                    setState(() {
+                      selectedIndex = 0;
+                    });
+                  },
+                  icon: const Icon(Icons.home)),
+              SizedBox(width: 25),
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      selectedIndex = 1;
+                    });
+                  },
+                  tooltip: "Mis turnos",
+                  icon: const Icon(Icons.event)),
+            ],
+          ),
+        ),
+        body: Expanded(
+          child: Container(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            child: page,
+          ),
         ),
       );
     });
   }
 }
 
-class GeneratorPage extends StatelessWidget {
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
+    // var appState = context.watch<MyAppState>();
+    // var pair = appState.current;
 
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
+    // IconData icon;
+    // if (appState.favorites.contains(pair)) {
+    //   icon = Icons.favorite;
+    // } else {
+    //   icon = Icons.favorite_border;
+    // }
 
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Me gusta'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Siguiente'),
-              ),
-            ],
-          ),
-        ],
-      ),
+    return GridView.count(
+      padding: const EdgeInsets.all(50),
+      crossAxisCount: 3,
+      crossAxisSpacing: 30,
+      mainAxisSpacing: 50,
+      childAspectRatio: 2,
+      children: [
+        for (int i = 0; i < 50; i++)
+          AppointmentCard(
+            appointment: Appointment(),
+          )
+      ],
     );
   }
 }
 
-class FavoritesPage extends StatelessWidget {
+class AppointmentCard extends StatelessWidget {
+  const AppointmentCard({
+    super.key,
+    required this.appointment,
+  });
+
+  final Appointment appointment;
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    return Container(
+      color: Theme.of(context).colorScheme.inversePrimary,
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Negocio: ${appointment.businessName}\nDescripcion: ${appointment.serviceDescription}\nPrecio: ${appointment.servicePrice.toStringAsFixed(2)}",
+            style: TextStyle(fontSize: 20),
+          ),
+          IconButton(
+              iconSize: 30,
+              onPressed: () {
+                appState.bookAppointment(appointment);
+              },
+              icon: const Icon(Icons.book))
+        ],
+      )),
+    );
+  }
+}
+
+class MyAppointmentsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
 
-    if (appState.favorites.isEmpty) {
+    if (appState.appointments.isEmpty) {
       return ListView(children: [
         Padding(
             padding: const EdgeInsets.all(20),
             child: Text(
-              'No hay favoritos todavía',
+              'No reservaste ningún turno aún',
               style: TextStyle(
-                  fontSize: 60, color: const Color.fromARGB(255, 8, 63, 49)),
+                  fontSize: 30, color: const Color.fromARGB(255, 8, 63, 49)),
             ))
       ]);
     }
 
-    int totalFavorites = appState.favorites.length;
+    int totalAppointments = appState.appointments.length;
 
     return ListView(
       children: [
         Padding(
             padding: const EdgeInsets.all(20),
             child: Text(
-                'Tenés $totalFavorites favorito${totalFavorites > 1 ? 's' : ''}:',
+                'Tenés $totalAppointments turno${totalAppointments > 1 ? 's' : ''}:',
                 style: TextStyle(
                     fontSize: 60,
                     color: const Color.fromARGB(255, 8, 63, 49)))),
-        for (var favorite in appState.favorites)
+        for (var appointment in appState.appointments)
           ListTile(
             leading: Icon(Icons.favorite),
-            title: Text(favorite.asPascalCase),
+            title: Text(
+                "${appointment.businessName}\n${appointment.serviceDescription}"),
           ),
       ],
     );
