474c2a1b109b59bfa268c7bbae3e164a8ba0af0c
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-11-05 00:34:34 +0000

    refactor: extraido sign in screen custom en nuevo widget

--- lib/widgets/pages/auth_gate.dart
+++ lib/widgets/pages/auth_gate.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
-import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:web_app/widgets/pages/custom_sign_in_screen.dart';
 import 'package:web_app/widgets/pages/my_home_page.dart';
 
 class AuthGate extends StatelessWidget {
@@ -12,33 +11,7 @@ class AuthGate extends StatelessWidget {
     return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
-        return snapshot.hasData
-            ? MyHomePage()
-            : SignInScreen(
-                actions: [
-                  AuthStateChangeAction(((context, state) {
-                    final user = switch (state) {
-                      SignedIn state => state.user,
-                      UserCreated state => state.credential.user,
-                      _ => null
-                    };
-                    if (user == null) {
-                      return;
-                    }
-                    if (state is UserCreated) {
-                      user.updateDisplayName(user.email!.split('@')[0]);
-                    }
-                    if (!user.emailVerified) {
-                      user.sendEmailVerification();
-                      const snackBar = SnackBar(
-                          content: Text(
-                              'Please check your email to verify your email address'));
-                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                    }
-                    context.goNamed('home');
-                  })),
-                ],
-              );
+        return snapshot.hasData ? MyHomePage() : CustomSignInScreen();
       },
     );
   }
