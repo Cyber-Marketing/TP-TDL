e2a8905a7b5694a90a46f1e2eeaad7f4968cd035
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-11-24 21:25:31 +0000

    fix: ahora la seccion my services para el usuario proveedor se actualiza en tiempo real

--- lib/widgets/pages/my_services_page.dart
+++ lib/widgets/pages/my_services_page.dart
@@ -6,41 +6,51 @@ import 'package:web_app/domain/service.dart';
 import 'package:web_app/widgets/cards/service_card.dart';
 import 'package:web_app/widgets/section_title.dart';
 
-class MyServicesPage extends StatelessWidget {
+class MyServicesPage extends StatefulWidget {
+  @override
+  State<MyServicesPage> createState() => _MyServicesPageState();
+}
+
+class _MyServicesPageState extends State<MyServicesPage> {
+  var _servicesStream = ServicesRepository().getServicesStream();
+
   @override
   Widget build(BuildContext context) {
     String userUid = context.watch<AppState>().currentUser!.uid;
 
-    return FutureBuilder(
-      future: ServicesRepository().getServicesByUser(userUid),
-      builder: ((context, snapshot) {
-        if (!snapshot.hasData) {
-          return Center(
-            child: CircularProgressIndicator(),
+    return StreamBuilder(
+        stream: _servicesStream,
+        builder: (BuildContext context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          var services = snapshot.data!.docs
+              .map((docSnapshot) {
+                var serviceMap = docSnapshot.data() as Map;
+                serviceMap['uid'] = docSnapshot.id;
+                return Service.fromMap(serviceMap);
+              })
+              .where((serv) => serv.ownerUid == userUid)
+              .toList();
+
+          String pluralSuffix = services.length > 1 ? 's' : '';
+          String sectionTitle = services.isEmpty
+              ? 'No has creado ningún servicio aún'
+              : 'Tenés ${services.length} servicio$pluralSuffix creado$pluralSuffix:';
+
+          return ListView(
+            padding: EdgeInsets.all(30),
+            children: [
+              SectionTitle(text: sectionTitle),
+              for (var service in services)
+                ServiceCard(
+                  service: service,
+                )
+            ],
           );
-        }
-        var services = snapshot.data!.docs.map((docSnapshot) {
-          var serviceMap = docSnapshot.data() as Map;
-          serviceMap['uid'] = docSnapshot.id;
-          return Service.fromMap(serviceMap);
-        }).toList();
-
-        String pluralSuffix = services.length > 1 ? 's' : '';
-        String sectionTitle = services.isEmpty
-            ? 'No has creado ningún servicio aún'
-            : 'Tenés ${services.length} servicio$pluralSuffix creado$pluralSuffix:';
-
-        return ListView(
-          padding: EdgeInsets.all(30),
-          children: [
-            SectionTitle(text: sectionTitle),
-            for (var service in services)
-              ServiceCard(
-                service: service,
-              )
-          ],
-        );
-      }),
-    );
+        });
   }
 }
