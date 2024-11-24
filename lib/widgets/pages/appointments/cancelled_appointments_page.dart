4d0174a8dd9343c9b1c2a643d80b4b0831984ef2
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-11-24 20:52:10 +0000

    fix: ahora las secciones cancelled y pending appointments tambien se actualizan en tiempo real

--- lib/widgets/pages/appointments/cancelled_appointments_page.dart
+++ lib/widgets/pages/appointments/cancelled_appointments_page.dart
@@ -6,44 +6,75 @@ import 'package:web_app/domain/appointment.dart';
 import 'package:web_app/widgets/cards/appointment_card.dart';
 import 'package:web_app/widgets/section_title.dart';
 
-class CancelledAppointmentsPage extends StatelessWidget {
+class CancelledAppointmentsPage extends StatefulWidget {
+  @override
+  State<CancelledAppointmentsPage> createState() =>
+      _CancelledAppointmentsPageState();
+}
+
+class _CancelledAppointmentsPageState extends State<CancelledAppointmentsPage> {
+  var _appointmentsStream = getAppointmentsStream();
+
   @override
   Widget build(BuildContext context) {
     String userUid = context.watch<AppState>().currentUser!.uid;
 
-    return FutureBuilder(
-      future: getUserCancelledAppointments(userUid),
-      builder: ((context, snapshot) {
-        if (!snapshot.hasData) {
-          return const Center(
-            child: CircularProgressIndicator(),
+    return StreamBuilder(
+        stream: _appointmentsStream,
+        builder: (BuildContext context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          var appointments = snapshot.data!.docs
+              .map((docSnapshot) {
+                var appointmentMap = docSnapshot.data();
+                appointmentMap['uid'] = docSnapshot.id;
+                return Appointment.fromMap(appointmentMap);
+              })
+              .where((app) =>
+                  app.getServiceDateTime().isAfter(DateTime.now()) &&
+                  app.isCancelled == true &&
+                  app.userUid == userUid)
+              .toList();
+
+          String sectionTitle = appointments.isEmpty
+              ? 'No cancelaste ningún turno aún'
+              : 'Cancelaste ${appointments.length} turno${appointments.length > 1 ? 's' : ''} :';
+
+          return ListView(
+            padding: EdgeInsets.all(30),
+            children: [
+              SectionTitle(text: sectionTitle),
+              for (var appointment in appointments)
+                AppointmentCard(
+                    appointment: appointment,
+                    userUid: userUid,
+                    isCancellable: false),
+            ],
           );
-        }
-        var appointments = snapshot.data!.docs
-            .map((docSnapshot) {
-              var appointmentMap = docSnapshot.data();
-              appointmentMap['uid'] = docSnapshot.id;
-              return Appointment.fromMap(appointmentMap);
-            })
-            .where((app) => app.getServiceDateTime().isAfter(DateTime.now()))
-            .toList();
+        });
 
-        String sectionTitle = appointments.isEmpty
-            ? 'No cancelaste ningún turno aún'
-            : 'Cancelaste ${appointments.length} turno${appointments.length > 1 ? 's' : ''} :';
+    // return FutureBuilder(
+    //   future: getUserCancelledAppointments(userUid),
+    //   builder: ((context, snapshot) {
+    //     if (!snapshot.hasData) {
+    //       return const Center(
+    //         child: CircularProgressIndicator(),
+    //       );
+    //     }
+    //     var appointments = snapshot.data!.docs
+    //         .map((docSnapshot) {
+    //           var appointmentMap = docSnapshot.data();
+    //           appointmentMap['uid'] = docSnapshot.id;
+    //           return Appointment.fromMap(appointmentMap);
+    //         })
+    //         .where((app) => app.getServiceDateTime().isAfter(DateTime.now()))
+    //         .toList();
 
-        return ListView(
-          padding: EdgeInsets.all(30),
-          children: [
-            SectionTitle(text: sectionTitle),
-            for (var appointment in appointments)
-              AppointmentCard(
-                  appointment: appointment,
-                  userUid: userUid,
-                  isCancellable: false),
-          ],
-        );
-      }),
-    );
+    //   }),
+    // );
   }
 }
--- lib/widgets/pages/appointments/pending_appointments_page.dart
+++ lib/widgets/pages/appointments/pending_appointments_page.dart
@@ -6,42 +6,53 @@ import 'package:web_app/domain/appointment.dart';
 import 'package:web_app/widgets/cards/appointment_card.dart';
 import 'package:web_app/widgets/section_title.dart';
 
-class PendingAppointmentsPage extends StatelessWidget {
+class PendingAppointmentsPage extends StatefulWidget {
+  @override
+  State<PendingAppointmentsPage> createState() =>
+      _PendingAppointmentsPageState();
+}
+
+class _PendingAppointmentsPageState extends State<PendingAppointmentsPage> {
+  var _appointmentsStream = getAppointmentsStream();
+
   @override
   Widget build(BuildContext context) {
     String userUid = context.watch<AppState>().currentUser!.uid;
 
-    return FutureBuilder(
-      future: getUserAppointments(userUid),
-      builder: ((context, snapshot) {
-        if (!snapshot.hasData) {
-          return Center(
-            child: CircularProgressIndicator(),
+    return StreamBuilder(
+        stream: _appointmentsStream,
+        builder: (BuildContext context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          var appointments = snapshot.data!.docs
+              .map((docSnapshot) {
+                var appointmentMap = docSnapshot.data();
+                appointmentMap['uid'] = docSnapshot.id;
+                return Appointment.fromMap(appointmentMap);
+              })
+              .where((app) =>
+                  !app.hasEnded() &&
+                  app.isCancelled == false &&
+                  app.userUid == userUid)
+              .toList();
+
+          String pluralSuffix = appointments.length > 1 ? 's' : '';
+          String sectionTitle = appointments.isEmpty
+              ? 'No tenés ningún turno pendiente'
+              : 'Tenés ${appointments.length} turno$pluralSuffix pendiente$pluralSuffix:';
+
+          return ListView(
+            padding: EdgeInsets.all(30),
+            children: [
+              SectionTitle(text: sectionTitle),
+              for (var appointment in appointments)
+                AppointmentCard(appointment: appointment, userUid: userUid)
+            ],
           );
-        }
-        var appointments = snapshot.data!.docs
-            .map((docSnapshot) {
-              var appointmentMap = docSnapshot.data();
-              appointmentMap['uid'] = docSnapshot.id;
-              return Appointment.fromMap(appointmentMap);
-            })
-            .where((app) => !app.hasEnded())
-            .toList();
-
-        String pluralSuffix = appointments.length > 1 ? 's' : '';
-        String sectionTitle = appointments.isEmpty
-            ? 'No tenés ningún turno pendiente'
-            : 'Tenés ${appointments.length} turno$pluralSuffix pendiente$pluralSuffix:';
-
-        return ListView(
-          padding: EdgeInsets.all(30),
-          children: [
-            SectionTitle(text: sectionTitle),
-            for (var appointment in appointments)
-              AppointmentCard(appointment: appointment, userUid: userUid)
-          ],
-        );
-      }),
-    );
+        });
   }
 }
