d7cdcb9ba751b3bbd637fe7596f29a3a8edb96b7
AbrilAilen <asimone@fi.uba.ar>
2024-11-25 09:38:13 +0000

    creo pagina para ver el feedback

---
+++ lib/widgets/pages/appointments/check_feedback_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:web_app/data/appointment_database.dart';
+import 'package:web_app/domain/appointment.dart';
+import 'package:web_app/domain/service.dart';
+import 'package:web_app/widgets/cards/service_feedback_card.dart';
+import 'package:web_app/widgets/non_home_app_bar.dart';
+import 'package:web_app/widgets/section_title.dart';
+
+class CheckFeedbackPage extends StatefulWidget {
+  CheckFeedbackPage({
+    super.key,
+    required this.service,
+  });
+
+  final Service service;
+
+  @override
+  State<CheckFeedbackPage> createState() => CheckFeedbackPageState();
+}
+
+class CheckFeedbackPageState extends State<CheckFeedbackPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+      appBar: NonHomeAppBar(context, text: "Ver opiniones del servicio"),
+      body: FutureBuilder(
+              future: getAppointmentsByBusinessName(widget.service.businessName),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                var appointments = snapshot.data!.docs
+                    .map((snapshotDoc) {
+                      try {
+                        var appointmentMap = snapshotDoc.data();
+                        appointmentMap['uid'] = snapshotDoc.id;
+                        return Appointment.fromMap(appointmentMap);
+                      } catch (e) {
+                        print('Error creating appointment: $e');
+                        return null;
+                      }
+                    })
+                    .where((app) =>
+                        app?.rating != null || app?.comment != null)
+                    .cast<Appointment>()
+                    .toList();
+
+                String sectionTitle = appointments.isEmpty
+                    ? 'Este servicio no ha recibido opiniones aún'
+                    : 'Hay ${appointments.length} opinion${appointments.length > 1 ? 'es' : ''} '
+                    'del servicio ${widget.service.businessName}:';
+
+                return ListView(
+                  padding: EdgeInsets.all(30),
+                  children: [
+                    SectionTitle(text: sectionTitle),
+                    for (var appointment in appointments)
+                      ServiceFeedbackCard(
+                          appointment: appointment),
+                  ],
+                );
+              },
+            )
+    );
+  }
+}
